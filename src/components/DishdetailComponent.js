bb64123c367b3bc273ac222534c49056d3d64b42
Priyansh <priyanshbhj@gmail.com>
2019-05-19 17:03:10 +0530

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -9,24 +9,26 @@ class DishDetail extends Component{
                 return(
                     <div tag="li">
                         <p>{comment.comment}</p>
-                        <p>-- {comment.author}, {(new Date(comment.date)).toDateString()}</p>
+                        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                     </div>
                 );
             });
             return(
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg src={dish.image} alt={dish.name} />
-                            <CardBody>
-                                <CardTitle>{dish.name}</CardTitle>
-                                <CardText>{dish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <div className="col-12 col-md-5 m-1" list>
-                        <h2>Comments</h2>
-                        {comments}
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <Card>
+                                <CardImg src={dish.image} alt={dish.name} />
+                                <CardBody>
+                                    <CardTitle>{dish.name}</CardTitle>
+                                    <CardText>{dish.description}</CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div className="col-12 col-md-5 m-1" list>
+                            <h2>Comments</h2>
+                            {comments}
+                        </div>
                     </div>
                 </div>
             );
