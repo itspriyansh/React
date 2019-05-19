d90630bc6d5401627e0d5348c2129450fe6a63a5
Priyansh <priyanshbhj@gmail.com>
2019-05-19 23:16:39 +0530

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,42 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardTitle, CardBody, CardText } from 'reactstrap';
 
-class DishDetail extends Component{
-    render() {
-        if(this.props.dish!=null){
-            const dish = this.props.dish;
-            const comments = dish.comments.map((comment) => {
-                return(
-                    <div tag="li">
-                        <p>{comment.comment}</p>
-                        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                    </div>
-                );
-            });
+const DishDetail = (props) => {
+    if(props.dish!=null){
+        const dish = props.dish;
+        const comments = dish.comments.map((comment) => {
             return(
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            <Card>
-                                <CardImg src={dish.image} alt={dish.name} />
-                                <CardBody>
-                                    <CardTitle>{dish.name}</CardTitle>
-                                    <CardText>{dish.description}</CardText>
-                                </CardBody>
-                            </Card>
-                        </div>
-                        <div className="col-12 col-md-5 m-1" list>
-                            <h2>Comments</h2>
-                            {comments}
-                        </div>
-                    </div>
+                <div tag="li">
+                    <p>{comment.comment}</p>
+                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                 </div>
             );
-        }else{
-            return(
-                <div></div>
-            );
-        }
+        });
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <Card>
+                            <CardImg src={dish.image} alt={dish.name} />
+                            <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className="col-12 col-md-5 m-1" list>
+                        <h2>Comments</h2>
+                        {comments}
+                    </div>
+                </div>
+            </div>
+        );
+    }else{
+        return(
+            <div></div>
+        );
     }
 }
 export default DishDetail;
